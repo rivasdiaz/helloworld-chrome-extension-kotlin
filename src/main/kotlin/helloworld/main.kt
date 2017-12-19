3fc7beabbb52af086e2824d447713a626fcc117a
Ramon Rivas <rivasdiaz@gmail.com>
2017-12-18 23:25:34 -0500

    extension main code updated to depend on browser.* APIs

--- src/main/kotlin/helloworld/main.kt
+++ src/main/kotlin/helloworld/main.kt
@@ -1,9 +1,7 @@
 package helloworld
 
-import chrome.storage.getSync
-import chrome.tabs.ExecuteScriptDetails
-import chrome.tabs.QueryInfo
-import chrome.tabs.querySync
+import browser.tabs.ExecuteScriptDetails
+import browser.tabs.QueryInfo
 import org.w3c.dom.HTMLSelectElement
 import kotlin.browser.document
 
@@ -15,14 +13,14 @@ import kotlin.browser.document
  */
 fun getCurrentTabUrlAsync() =
         async {
-            // Query filter to be passed to chrome.tabs.query - see
-            // https://developer.chrome.com/extensions/tabs#method-query
+            // Query filter to be passed to browser.tabs.query - see
+            // https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/API/tabs/query
             val queryInfo = QueryInfo {
                 active = true
                 currentWindow = true
             }
-            val tabs = querySync(queryInfo)
-            // chrome.tabs.query invokes the callback with a list of tabs that match the
+            val tabs = browser.tabs.query(queryInfo).await()
+            // browser.tabs.query invokes the callback with a list of tabs that match the
             // query. When the popup is opened, there is certainly a window and at least
             // one tab, so we can safely assume that |tabs| is a non-empty array.
             // A window can only have one active tab at a time, so the array consists of
@@ -30,7 +28,7 @@ fun getCurrentTabUrlAsync() =
             val tab = tabs[0]
 
             // A tab is a plain object that provides information about the tab.
-            // See https://developer.chrome.com/extensions/tabs#type-Tab
+            // See https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/API/tabs/Tab
             val url = tab.url
             // tab.url is only available if the "activeTab" permission is declared.
             // If you want to see the URL of other tabs (e.g. after removing active:true
@@ -46,12 +44,12 @@ fun getCurrentTabUrlAsync() =
  */
 fun changeBackgroundColor(color: String) {
     val script = """document.body.style.backgroundColor="$color";"""
-    // See https://developer.chrome.com/extensions/tabs#method-executeScript.
-    // chrome.tabs.executeScript allows us to programmatically inject JavaScript
+    // See https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/API/tabs/executeScript.
+    // browser.tabs.executeScript allows us to programmatically inject JavaScript
     // into a page. Since we omit the optional first argument "tabId", the script
     // is inserted into the active tab of the current window, which serves as the
     // default.
-    chrome.tabs.executeScript(
+    browser.tabs.executeScript(
             details = ExecuteScriptDetails {
                 code = script
             }
@@ -62,15 +60,13 @@ fun changeBackgroundColor(color: String) {
  * Gets the saved background color for url.
  *
  * @param url URL whose background color is to be retrieved.
- * @param callback called with the saved background color for
- *     the given url on success, or a falsy value if no color is retrieved.
+ * @param promise fulfilled with the saved background color for
+ *     the given url on success, or rejected if no color is retrieved.
  */
 fun getSavedBackgroundColorAsync(url: String) =
         async {
-            // See https://developer.chrome.com/apps/storage#type-StorageArea. We check
-            // for chrome.runtime.lastError to ensure correctness even when the API call
-            // fails.
-            val items = chrome.storage.sync.getSync(url)
+            // See https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/API/storage/StorageArea.
+            val items = browser.storage.sync.get(url).await()
             items[url] as? String
         }
 
@@ -83,19 +79,19 @@ fun getSavedBackgroundColorAsync(url: String) =
 fun saveBackgroundColor(url: String, color: String) {
     val items = js("{}")
     items[url] = color
-    // See https://developer.chrome.com/apps/storage#type-StorageArea. We omit the
-    // optional callback since we don't need to perform any action once the
+    // See https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/API/storage/StorageArea.
+    // We do not use the returned Promise since we don't need to perform any action once the
     // background color is saved.
-    chrome.storage.sync.set(items)
+    browser.storage.sync.set(items)
 }
 
 // This extension loads the saved background color for the current tab if one
 // exists. The user can select a new background color from the dropdown for the
 // current page, and it will be saved as part of the extension's isolated
-// storage. The chrome.storage API is used for this purpose. This is different
+// storage. The browser.storage API is used for this purpose. This is different
 // from the window.localStorage API, which is synchronous and stores data bound
-// to a document's origin. Also, using chrome.storage.sync instead of
-// chrome.storage.local allows the extension data to be synced across multiple
+// to a document's origin. Also, using browser.storage.sync instead of
+// browser.storage.local allows the extension data to be synced across multiple
 // user devices.
 fun main(args: Array<String>) {
     document.onContentLoadedEventAsync {
